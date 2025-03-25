8a79c90fe86b460d5dfced2296b1b20791904bf9
Dang Khoa <i.am@dangkhoa.dev>
2025-03-25 11:19:57 +0000

    refactor devise and layout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,25 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
+  before_action :authenticate_user!, unless: :skip_verify_user?
+
+  layout :get_layout
+
+  private
+
+  def get_layout
+    if devise_controller? && !is_edit_profile?
+      "devise"
+    else
+      "application"
+    end
+  end
+
+  def is_edit_profile?
+    controller_name == "registrations" && action_name.in?([ "edit", "update" ])
+  end
+
+  def skip_verify_user?
+    controller_name == "home"
+  end
 end
