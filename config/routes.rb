e8c96f72e316b57c76901b5e27e3a9f6e0c6bd16
Dang Khoa <i.am@dangkhoa.dev>
2025-03-26 09:50:47 +0000

    add home page

--- config/routes.rb
+++ config/routes.rb
@@ -25,9 +25,11 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
-  # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
+  get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
   # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
 
+  get "home/index"
+
   # Defines the root path route ("/")
-  # root "posts#index"
+  root "home#index"
 end
