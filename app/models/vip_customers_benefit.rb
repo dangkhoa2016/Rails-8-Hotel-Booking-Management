b65f8083d6eafb0f110a3a258120d762d3477939
Trần Đăng Khoa <khoa.tran@readytech.io>
2025-03-27 09:19:18 +0000

    dynamic render model show detail

--- app/models/vip_customers_benefit.rb
+++ app/models/vip_customers_benefit.rb
@@ -1,7 +1,6 @@
 class VipCustomersBenefit < ApplicationRecord
   belongs_to :customer
 
-
   enum :discount_type, { percent: 0, amount: 1 }
   enum :status, { inactive: 0, active: 1 }
 
@@ -20,6 +19,10 @@ class VipCustomersBenefit < ApplicationRecord
 
   def to_s
     "Customer: #{customer&.name} <> #{discount_type.humanize}"
+  end
+
+  def display_at_index_page_columns
+    %w[customer_id discount_type discount_percent_on_room_price discount_amount_on_room_price status]
   end
 
   private
