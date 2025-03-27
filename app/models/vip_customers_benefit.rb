743a1ac5c43d041c6963c1ad9b16697959dccb75
Trần Đăng Khoa <khoa.tran@readytech.io>
2025-03-27 08:57:43 +0000

    update render card header for models

--- app/models/vip_customers_benefit.rb
+++ app/models/vip_customers_benefit.rb
@@ -4,4 +4,9 @@ class VipCustomersBenefit < ApplicationRecord
 
   enum :discount_type, { percent: 0, amount: 1 }
   enum :status, { inactive: 0, active: 1 }
+
+
+  def to_s
+    "Customer: #{customer&.name} <> #{discount_type.humanize}"
+  end
 end
