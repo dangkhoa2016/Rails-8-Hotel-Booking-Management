bd34bdf1c2327ad543451b8bc46c833d715b2db3
Trần Đăng Khoa <khoa.tran@readytech.io>
2025-03-29 14:28:58 +0000

    refactor model validates and forms

--- app/models/booking_vip_customer_discount.rb
+++ app/models/booking_vip_customer_discount.rb
@@ -6,7 +6,27 @@ class BookingVipCustomerDiscount < ApplicationRecord
   enum :discount_type, { percent: 0, amount: 1 }
 
 
+  validates :discount_type, presence: true
+  validates :discount_percent_on_room_price, numericality: { greater_than: 0, less_than_or_equal_to: 100 }, allow_blank: true, if: -> { discount_type == "percent" }
+  validates :discount_percent_on_additional_services, numericality: { greater_than: 0, less_than_or_equal_to: 100 }, allow_blank: true, if: -> { discount_type == "percent" }
+  validates :discount_amount_on_room_price, numericality: { greater_than: 0 }, allow_blank: true, if: -> { discount_type == "amount" }
+  validates :discount_amount_on_additional_services, numericality: { greater_than: 0 }, allow_blank: true, if: -> { discount_type == "amount" }
+  validates :booking_id, uniqueness: { scope: [:customer_id, :discount_type] }
+
+  # Custom validation to ensure that either discount_percent or discount_amount is present
+  validate :at_least_one_discount_present
+
+
   def to_s
     "Booking ##{booking_id} - Customer ##{customer_id}"
+  end
+  
+  private
+
+  def at_least_one_discount_present
+    if discount_percent_on_room_price.blank? && discount_percent_on_additional_services.blank? &&
+        discount_amount_on_room_price.blank? && discount_amount_on_additional_services.blank?
+      errors.add(:base, "At least one discount must be present")
+    end
   end
 end
