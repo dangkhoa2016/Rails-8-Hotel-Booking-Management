e9637401cd01e912fb91741cfb7198375bcd67cf
Dang Khoa <i.am@dangkhoa.dev>
2025-04-03 10:28:17 +0000

    update card header render

--- app/helpers/vip_customers_benefits_helper.rb
+++ app/helpers/vip_customers_benefits_helper.rb
@@ -1,2 +1,17 @@
 module VipCustomersBenefitsHelper
+  def render_vip_customers_benefit_discount_percent_on_room_price(record)
+    record.discount_percent_on_room_price.display_as_percentage
+  end
+
+  def render_vip_customers_benefit_discount_percent_on_additional_services(record)
+    record.discount_percent_on_additional_services.display_as_money
+  end
+
+  def render_vip_customers_benefit_discount_amount_on_room_price(record)
+    record.discount_amount_on_room_price.display_as_money
+  end
+
+  def render_vip_customers_benefit_discount_amount_on_additional_services(record)
+    record.discount_amount_on_additional_services.display_as_money
+  end
 end
---
+++ config/initializers/money_formats.rb
@@ -0,0 +1,21 @@
+module MoneyFormats
+  include ActionView::Helpers::NumberHelper
+
+  def display_as_money(options = {})
+    en_options = I18n.t("number.currency.format", locale: "en")
+    precision = self.to_i == self ? 0 : en_options[:precision]
+    options.merge!(unit: en_options[:unit], precision: precision)
+    number_to_currency(self, options)
+  end
+
+  def display_as_percentage(options = {})
+    en_options = I18n.t("number.currency.format.precision", locale: "en")
+    precision = self.to_i == self ? 0 : en_options
+    options.merge!(unit: "%", precision: precision)
+    number_to_percentage(self, options)
+  end
+end
+
+[ NilClass, Float, Integer, BigDecimal ].each do |klass|
+  klass.include(MoneyFormats)
+end
