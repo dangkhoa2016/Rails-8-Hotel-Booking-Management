313bc3a1329bdb0b70ee2748f90cc49cc81c9ea1
Trần Đăng Khoa <khoa.tran@readytech.io>
2025-04-03 10:28:17 +0000

    update card header render

--- app/helpers/booking_discounts_helper.rb
+++ app/helpers/booking_discounts_helper.rb
@@ -1,2 +1,9 @@
 module BookingDiscountsHelper
+  def render_booking_discount_discount_percent(record)
+    record.discount_percent.display_as_percentage
+  end
+
+  def render_booking_discount_discount_amount(record)
+    record.discount_amount.display_as_money
+  end
 end
