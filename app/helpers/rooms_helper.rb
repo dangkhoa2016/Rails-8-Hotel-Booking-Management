2e0610fe0ea30929175f6fa1af44cc080ede49c2
Dang Khoa <i.am@dangkhoa.dev>
2025-04-06 10:34:49 +0000

    ability to update and show room and facility mapping

--- app/helpers/rooms_helper.rb
+++ app/helpers/rooms_helper.rb
@@ -4,4 +4,11 @@ module RoomsHelper
     return value if value.nil?
     Room.human_attribute_name("available.#{value ? "active" : "inactive"}")
   end
+
+  def room_html_data_attributes(record)
+    {
+      controller: "border-color",
+      border_color: record.room_type.color
+    }
+  end
 end
