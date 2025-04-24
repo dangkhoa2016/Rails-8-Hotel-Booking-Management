2afcf5684a46701ca04d972badf4003f3ad42f80
Dang Khoa <i.am@dangkhoa.dev>
2025-04-24 09:12:13 +0000

    show room associations

--- app/models/room.rb
+++ app/models/room.rb
@@ -3,9 +3,10 @@ class Room < ApplicationRecord
   belongs_to :room_type
   belongs_to :hotel_location
 
-  has_many :bookings
+  has_many :reservations
   has_many :facilities_rooms
   has_many :facilities, through: :facilities_rooms
+  has_many :reviews
 
 
   enum :status, { inactive: 0, active: 1 }
